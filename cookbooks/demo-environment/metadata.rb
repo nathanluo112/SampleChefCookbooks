34044b56fdd2fdb13d2339fef26db7b13e1b9d2d
Nathan <nathan.luo@parivedasolutions.com>
2016-06-24 17:12:56 -0400

    Change metadata.rb

--- cookbooks/demo-environment/metadata.rb
+++ cookbooks/demo-environment/metadata.rb
@@ -2,6 +2,6 @@ name 'demo-environment'
 maintainer 'GameStop'
 maintainer_email 'admin@gamestop.com'
 license 'all_rights'
-description 'Change file content base on environment'
+description 'Demo cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
