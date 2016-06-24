a569d10f68b613617e03118e48d4ad74e6f19d94
Nathan <nathan.luo@parivedasolutions.com>
2016-06-24 09:53:03 -0400

    Add demo cookbook

---
+++ cookbooks/demo-environment/metadata.rb
@@ -0,0 +1,7 @@
+name 'demo-environment'
+maintainer 'GameStop'
+maintainer_email 'admin@gamestop.com'
+license 'all_rights'
+description 'Change file content base on environment'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '1.0.0'
---
+++ cookbooks/demo-environment/recipes/default.rb
@@ -0,0 +1,7 @@
+# Reboot machine
+
+file 'C:\tmp\environment-demo.txt' do
+  content node["demo"]["environment"]
+  action :create
+  not_if { ::File.exists?('C:\tmp\environment-demo.txt') } 
+end
